2b24dc531762724d7def0628a92a177f4cd88f8c
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2017-09-15 09:02:21 -0600

    Created the monster and added setters and getters

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,9 +1,13 @@
 package monster.controller;
 
+import monster.model.MarshmallowMonster;
+
 public class MonsterController
 {
 	public void start()
 	{
-		
+		MarshmallowMonster sample = new MarshmallowMonster();
+		System.out.println(sample);
+		MarshmallowMonster realMonster = new MarshmallowMonster("Jerry", 2, 10, 8, true);
 	}
 }
